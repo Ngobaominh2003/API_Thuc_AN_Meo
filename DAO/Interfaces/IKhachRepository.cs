b6210c340d1de38592a776e5f422f0cdaef65423
Ngobaominh2003 <dongchieulienphuong.hy@gmail.com>
2023-10-17 21:39:27 +0700

    o

--- DAO/Interfaces/IKhachRepository.cs
+++ DAO/Interfaces/IKhachRepository.cs
@@ -1,17 +1,17 @@
-﻿using DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using DTO;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace DAO
-{
-    public partial interface IKhachRepository
+    namespace DAO
     {
-        KhachHang GetDatabyID(string id);
-        bool Create(KhachHang model);
-        bool Update(KhachHang model);
-        public List<KhachHang> Search(int pageIndex, int pageSize, out long total, string ten_khach, string dia_chi);
+        public partial interface IKhachRepository
+        {
+            KhachHang GetDatabyID(string id);
+            bool Create(KhachHang model);
+            bool Update(KhachHang model);
+            public List<KhachHang> Search(int pageIndex, int pageSize, out long total, string ten_khach, string dia_chi);
+        }
     }
-}
